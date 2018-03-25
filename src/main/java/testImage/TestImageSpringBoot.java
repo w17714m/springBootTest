4ae527e446d5222292b9523feee17ebc673bcb90
w17714m <jwilli0508@gmail.com>
2018-03-24 23:00:58 -0500

    primer commit

---
+++ src/main/java/testImage/TestImageSpringBoot.java
@@ -0,0 +1,13 @@
+package testImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestImageSpringBoot {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TestImageSpringBoot.class, args);
+	}
+	
+}
---
+++ src/main/java/testImage/controller.java
@@ -0,0 +1,76 @@
+package testImage;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import javax.swing.JOptionPane;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import OnBarcode.Barcode.BarcodeScanner.BarcodeScanner;
+import OnBarcode.Barcode.BarcodeScanner.BarcodeType;
+
+@Controller
+@RequestMapping("/api/")
+public class controller {
+
+	@PostMapping("/test/")
+	public ResponseEntity<String> getDataCC(){
+		
+		String []  barcodes = BarcodeScanner.Scan("", BarcodeType.PDF417);
+		
+		if(barcodes.length>0) {
+			System.out.println("longitud" + barcodes[0].toString());
+		}else {
+			System.out.println("No devolvio nada" );
+		}
+		
+		return new ResponseEntity<String>(barcodes[0].toString(),HttpStatus.OK);
+	}
+	@PostMapping("/archivo/")
+	public ResponseEntity<String> getDataFile(@RequestParam("archivo") MultipartFile file){
+		try {
+			File ft = saveFile(file);
+			if(ft.exists()) {
+				System.out.println("EL ARCHIVO EXISTE");
+			}else
+			{
+				System.out.println("NO EXISTE");
+			}
+			
+			System.out.println("LA RUTA ABSOLUTA ES: -->"+ft.getAbsolutePath());
+			String []  barcodes = BarcodeScanner.Scan(ft.getAbsolutePath(), BarcodeType.PDF417);
+			
+			if(barcodes.length>0) {
+				System.out.println("longitud" + barcodes[0].toString());
+			}else {
+				System.out.println("No devolvio nada" );
+			}
+			
+			return new ResponseEntity<String>(barcodes[0].toString(),HttpStatus.OK);
+			
+			
+		} catch (Exception e) {
+			System.out.println("Error no identificado" + e.getStackTrace());
+			return null;
+		}
+	}
+	
+	
+	private File saveFile(MultipartFile mpf) throws Exception {
+	    File convFile = new File( mpf.getOriginalFilename());
+	    convFile.createNewFile();
+	    FileOutputStream fos = new FileOutputStream(convFile); 
+	    fos.write(mpf.getBytes());
+	    fos.close(); 
+	    return convFile;
+	}
+	
+}
